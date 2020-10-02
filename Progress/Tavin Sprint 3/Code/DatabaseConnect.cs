d839548c9e35c54b823d8e62371088e4439b168d
Tavin Head <tavinh@clemson.edu>
2020-10-02 06:27:03 +0000

    Added 12 files to /Progress/Tavin Sprint 3/Code

---
+++ Progress/Tavin Sprint 3/Code/DatabaseConnect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MyAccountPage
+{
+    class DatabaseConnect
+    {
+        private DatabaseConnect()
+        {
+        }
+
+        public string DatabaseName { get; set; } = "infTest_fel7";
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DatabaseConnect _instance = null;
+        public static DatabaseConnect Instance()
+        {
+            if (_instance == null)
+                _instance = new DatabaseConnect();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null || Connection.State != System.Data.ConnectionState.Open)
+            {
+                if (String.IsNullOrEmpty(DatabaseName))
+                    return false;
+
+                string connstring = string.Format("Server=mysql1.cs.clemson.edu; database={0}; UID=infTest_045z; password=B00zer01", DatabaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+            connection = null;
+        }
+    }
+}
---
+++ Progress/Tavin Sprint 3/Code/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace MyAccountPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            //Saves the updated data
+        }
+
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            //Adds the item to your cart
+        }
+
+        private void Cart_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigates to the cart page
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
